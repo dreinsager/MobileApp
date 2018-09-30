e3de75aaaee3df708079407dee24f94f7f62a1e7
Doron <dreinsager@gmail.com>
2018-09-30 13:23:00 -0500

    navigate to dice page

--- MobileAppProject/src/pages/home/home.ts
+++ MobileAppProject/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
+import { TestPage } from '../test/test';
 
 
 @Component({
@@ -8,16 +8,11 @@ import { AlertController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController) {
 
   }
-  showAlert() {
-    const alert = this.alertCtrl.create({
-      title: 'To Dice!',
-      subTitle: 'This will take you to the dice roller page where we will make a random number generator to output certain numbers on the die',
-      buttons: ['OK']
-    });
-    alert.present();
-  }
+  toDiceRoll(){
+    this.navCtrl.push(TestPage);
+  } 
 
 }
