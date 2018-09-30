ae5d8df69094fbb4e29856c9a190ede889c74cd3
Doron <dreinsager@gmail.com>
2018-09-30 13:54:21 -0500

    fixed dice roll page to where it has back button

--- MobileAppProject/src/app/app.component.ts
+++ MobileAppProject/src/app/app.component.ts
@@ -24,8 +24,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'Test', component: TestPage },
-      { title: 'List', component: ListPage }
+      { title: 'Dice Roll', component: TestPage },
+      { title: 'Character', component: ListPage }
     ];
 
   }
