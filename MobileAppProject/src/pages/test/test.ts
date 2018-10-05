f0026b5f47bce7577a1822baa6cc66a0c636ec60
dreinsager <dreinsager@gmail.com>
2018-10-05 13:16:33 -0500

    added dice rolls to dice page

--- MobileAppProject/src/pages/test/test.ts
+++ MobileAppProject/src/pages/test/test.ts
@@ -14,9 +14,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'test.html',
 })
 export class TestPage {
+  randomNumber4;
+  randomNumber6;
+  randomNumber8;
+  randomNumber10;
+  randomNumber12;
+  randomNumber20;
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+
+  getD4(){
+    this.randomNumber4 = Math.ceil(Math.random() * 4)
+  }
+
+  getD6(){
+    this.randomNumber6 = Math.ceil(Math.random() * 6)
+  }
+
+  getD8(){
+    this.randomNumber8 = Math.ceil(Math.random() * 8)
+  }
+
+  getD10(){
+    this.randomNumber10 = Math.ceil(Math.random() * 10)
+  }
+  getD12(){
+    this.randomNumber12 = Math.ceil(Math.random() * 12)
+  }
+  getD20(){
+    this.randomNumber20 = Math.ceil(Math.random() * 20)
+  }
+
+
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TestPage');
